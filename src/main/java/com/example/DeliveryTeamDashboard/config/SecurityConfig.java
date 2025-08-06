7344aba7e4137b0eb9cdd4cbf0c1ec62575eac24
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2025-08-06 19:26:10 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/DeliveryTeamDashboard/config/SecurityConfig.java
+++ src/main/java/com/example/DeliveryTeamDashboard/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://frontend:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
